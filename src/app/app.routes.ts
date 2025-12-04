e670ebf58d37513b07936cda1a072bb381836ffb
Júlia Pimentel <juliapimentelma@gmail.com>
2025-12-04 18:22:26 -0300

    ajustes de

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
+import { Products } from './products/products'; // A classe que você criou
+import { AboutMe } from './about-me/about-me';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomePage },
+
+    { path: 'produtos', component: Products },
+
+    { path: 'sobre', component: AboutMe },
+
+    { path: 'contato', component: Contact },
+
+    { path: '**', redirectTo: '' } 
+];
+
--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
+
 export class Products {
+
+  listaProdutos = [
+    {
+      nome: 'case nuvem',
+      imagem: '../../imagens/001.png',
+      categoria: 'case'
+    },
+    {
+      nome: 'case costela',
+      imagem: '../../imagens/bg2.png',
+      categoria: 'case'
+    },
+    {
+      nome: 'case sailor moon',
+      imagem: '../../imagens/003.png',
+      categoria: 'case'
+    }
+  ];
 
 }
